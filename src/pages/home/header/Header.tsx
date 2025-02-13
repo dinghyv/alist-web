6ba26c8ab4ea93e403e8cbc24e039da1d3b188a2
dinghy421 <dinghyv@outlook.com>
2025-02-13 15:24:34 +0800

    Revert "1"
    This reverts commit 8330faeb28b59c6bf5e8a117366175972dc58269.

--- src/pages/home/header/Header.tsx
+++ src/pages/home/header/Header.tsx
@@ -83,6 +83,7 @@ export const Header = () => {
                 </HStack>
               </Show>
               <DinghyHomepage />
+              <SendMeEmail />
               <SwitchLanguageWhite />
               <Layout />
             </Show>
