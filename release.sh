4c3f4d5c78fc3200c1c7807dc3907c03c524102b
dinghyv <dinghyv@gmail.com>
2025-07-12 09:44:10 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,6 +7,17 @@ cat package.json
 # build
 pnpm install
 pnpm i18n:release
+wget https://crowdin.com/backend/download/project/alist/zh-CN.zip // [!code ++]
+unzip zh-CN.zip // [!code ++]
+wget https://crowdin.com/backend/download/project/alist/zh-TW.zip // [!code ++]
+unzip zh-TW.zip // [!code ++]
+wget https://crowdin.com/backend/download/project/alist/ja.zip // [!code ++]
+unzip ja.zip // [!code ++]
+node ./scripts/i18n.mjs // [!code ++]
+rm zh-CN.zip // [!code ++]
+rm zh-TW.zip // [!code ++]
+rm ja.zip // [!code ++]
+
 pnpm build
 cp -r dist ../
 cd ..
